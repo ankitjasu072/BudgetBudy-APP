f5bd869122a4967844b467ce6056aba45c3e6b3c
Ankit Jasu <ankit@Ankits-MacBook-Air.local>
2022-08-11 22:17:53 +0530

    initial commit

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,86 @@
+import 'package:daily_spending/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AppDrawer extends StatelessWidget {
+  final int total;
+
+  AppDrawer({this.total});
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Daily Spendings',
+            style: Theme.of(context).appBarTheme.textTheme.headline1,
+          ),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.home),
+                    title: const Text("Home"),
+                    onTap: () {
+                      Navigator.of(context)
+                          .pushReplacementNamed(HomeScreen.routeName);
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.mail),
+                    title: Text("Contact Us"),
+                    onTap: () async {
+                      String url = Uri.encodeFull(
+                          "mailto:ankitjasu050@gmail.com?subject=NeedHelp&body=Contact Reason: ");
+                      if (await canLaunch(url)) {
+                        Navigator.of(context).pop();
+                        await launch(url);
+                      }
+                    },
+                  ),
+                  ListTile(
+                    leading: Image.asset(
+                      'assets/images/github.png',
+                      color: Colors.grey,
+                    ),
+                    title: Text("Contribute"),
+                    onTap: () async {
+                      String url = Uri.encodeFull(
+                          "https://github.com/ankitjasu072");
+                      if (await canLaunch(url)) {
+                        Navigator.of(context).pop();
+                        await launch(url);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    'Total: ₹$total',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
